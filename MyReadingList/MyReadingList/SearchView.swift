9f47a88d6f36610ec60d68734119965c3244b5ad
ivanovakatya <katyaivanova0799@gmail.com>
2021-11-06 23:37:18 +0000

    Change localization key from 'ScreenView' to 'screenView' for consistency

--- MyReadingList/MyReadingList/SearchView.swift
+++ MyReadingList/MyReadingList/SearchView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct SearchView: View {
     var body: some View {
-        Text(NSLocalizedString("ui.SearchView.title", value: "Search Screen", comment: "title for the Search screen"))
+        Text(NSLocalizedString("ui.searchView.title", value: "Search Screen", comment: "title for the Search screen"))
             .font(.headline)
         Spacer()
     }
