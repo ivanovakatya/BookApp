ec1305eb51df4081d310fb4262838ba7680de1c2
ivanovakatya <katyaivanova0799@gmail.com>
2021-12-04 12:38:19 +0000

    Add Navigation view to 'SearchView' file

--- MyReadingList/MyReadingList/SearchView.swift
+++ MyReadingList/MyReadingList/SearchView.swift
@@ -9,10 +9,12 @@ import SwiftUI
 
 struct SearchView: View {
     var body: some View {
-        VStack{
-            Text(NSLocalizedString("ui.search.title", value: "Search", comment: "title for the Search screen"))
+        NavigationView{
+            VStack{
+                Text(NSLocalizedString("ui.search.title", value: "Search", comment: "title for the Search screen"))
                 .font(.headline)
-            Spacer()
+                Spacer()
+            }
         }
     }
 }
