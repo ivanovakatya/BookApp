a3b65a455bbc37d6338ffb51064594cac58cf86b
ivanovakatya <katyaivanova0799@gmail.com>
2021-12-12 16:43:55 +0000

    BA-4 Removes the unnecessary VStack and replacing it with HStack

--- MyReadingList/MyReadingList/MyReadingListView.swift
+++ MyReadingList/MyReadingList/MyReadingListView.swift
@@ -11,15 +11,13 @@ import SwiftUI
 struct MyReadingListView: View {
     var body: some View {
         NavigationView {
-            VStack {
+            HStack {
                 List {
-                    HStack{
-                        NavigationLink {
-                            SearchView()
-                        } label: {
-                            Image(systemName: "plus.circle")
-                            Text(NSLocalizedString("ui.myReadingList.addABook.button", value: "Add a book", comment: "Add a Book button in MyReadingList screen"))
-                        }
+                    NavigationLink {
+                        SearchView()
+                    } label: {
+                        Image(systemName: "plus.circle")
+                        Text(NSLocalizedString("ui.myReadingList.addABook.button", value: "Add a book", comment: "Add a Book button in MyReadingList screen"))
                     }
                 }
             }.navigationTitle(NSLocalizedString("ui.myReadingList.title", value: "My Reading List", comment: "title for the MyReadingList screen"))
