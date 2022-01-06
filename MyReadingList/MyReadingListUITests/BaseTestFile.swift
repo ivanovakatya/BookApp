30dcddd1244ef98e4f54ce9ae81cb98676b764d7
ivanovakatya <katyaivanova0799@gmail.com>
2022-01-06 22:49:52 +0000

    Creates the BaseTestClass

---
+++ MyReadingList/MyReadingListUITests/BaseTestFile.swift
@@ -0,0 +1,13 @@
+//
+//  BaseTestFile.swift
+//  MyReadingListUITests
+//
+//  Created by MacBook on 06/01/2022.
+//
+
+import Foundation
+import XCTest
+
+class BaseTestClass: XCTestCase {
+    
+}
