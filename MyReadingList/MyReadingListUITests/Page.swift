6b1ac7a5e7d27ddb72c099f4262cb7677ee65561
ivanovakatya <katyaivanova0799@gmail.com>
2022-01-11 14:57:18 +0000

    Add protocol Page

---
+++ MyReadingList/MyReadingListUITests/Page.swift
@@ -0,0 +1,13 @@
+//
+//  Page.swift
+//  MyReadingListUITests
+//
+//  Created by MacBook on 11/01/2022.
+//
+
+import Foundation
+import XCTest
+
+protocol Page {
+    var app: XCUIApplication { get }
+}
