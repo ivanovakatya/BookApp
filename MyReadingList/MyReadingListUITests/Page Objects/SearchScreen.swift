2729ba8debec87851c29f98747c1974a18475ade
ivanovakatya <katyaivanova0799@gmail.com>
2022-01-14 23:33:47 +0000

    Add tests in SearchScreen to be able to test the SearchView title

--- MyReadingList/MyReadingListUITests/Page Objects/SearchScreen.swift
+++ MyReadingList/MyReadingListUITests/Page Objects/SearchScreen.swift
@@ -5,4 +5,24 @@
 //  Created by MacBook on 14/01/2022.
 //
 
-import Foundation
+import XCTest
+
+class SearchScreen: Page {
+    var app: XCUIApplication
+
+    init() {
+        app = XCUIApplication()
+    }
+
+    var back: XCUIElement {
+        return app.navigationBars.buttons.element(boundBy: 0)
+    }
+    
+    var searchTitle: XCUIElement {
+        return app.navigationBars["Search"].firstMatch
+    }
+    
+    var navbar: XCUIElement {
+        return app.navigationBars.firstMatch
+    }
+}
