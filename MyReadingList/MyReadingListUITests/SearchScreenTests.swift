9146a12928f10acfe54ab5d116aa6e66b1fef3f5
ivanovakatya <katyaivanova0799@gmail.com>
2022-01-18 22:05:19 +0000

    Create SearchScreenTest class

---
+++ MyReadingList/MyReadingListUITests/SearchScreenTests.swift
@@ -0,0 +1,13 @@
+//
+//  SearchScreenTests.swift
+//  MyReadingListUITests
+//
+//  Created by MacBook on 18/01/2022.
+//
+
+import XCTest
+import MyReadingList
+
+class SearchScreenTests: BaseTestFile {
+    let myReadingListScreen = SearchScreen()
+}
