6482ce6373dbd15a78f36b58b5b6652fa537fdae
ivanovakatya <katyaivanova0799@gmail.com>
2022-01-23 13:17:08 +0000

    Clean the Search view

--- MyReadingList/MyReadingList/SearchView.swift
+++ MyReadingList/MyReadingList/SearchView.swift
@@ -9,19 +9,12 @@ import SwiftUI
 
 
 struct SearchView: View {
-    @Environment(\.dismissSearch) var dismissSearch
+    @Environment(\.isSearching) var isSearching
     @State private var searchText = ""
 
     var body: some View {
-        VStack{
-            Text("")
-                .accessibility(identifier: "search-book-button")
-        }
-        .searchable(text: $searchText, placement: .navigationBarDrawer(displayMode: .always), prompt: "Search for a book")
         .navigationTitle(NSLocalizedString("ui.search.title", value: "Search", comment: "title for the Search screen"))
-        .onSubmit(of: .search) {
-            dismissSearch()
-        }
+        .searchable(text: $searchText, placement: .navigationBarDrawer(displayMode: .always), prompt: "Search for a book")
     }
 }
 
