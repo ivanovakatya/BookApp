a37fb2ee63940bd1a3a6b582fe8ee5c1c7a9cfbb
ivanovakatya <katyaivanova0799@gmail.com>
2022-01-23 13:38:11 +0000

    Add new file 'SearchBody'

---
+++ MyReadingList/MyReadingList/SearchBody.swift
@@ -0,0 +1,18 @@
+//
+//  SearchBody.swift
+//  MyReadingList
+//
+//  Created by MacBook on 23/01/2022.
+//
+
+import SwiftUI
+
+struct SearchBody: View {
+    @Environment(\.isSearching) var isSearching
+
+    var body: some View {
+        VStack {
+            // TODO: Search Results list goes here
+        }
+    }
+}
--- MyReadingList/MyReadingList/SearchView.swift
+++ MyReadingList/MyReadingList/SearchView.swift
@@ -9,12 +9,12 @@ import SwiftUI
 
 
 struct SearchView: View {
-    @Environment(\.isSearching) var isSearching
     @State private var searchText = ""
-
+    
     var body: some View {
-        .navigationTitle(NSLocalizedString("ui.search.title", value: "Search", comment: "title for the Search screen"))
-        .searchable(text: $searchText, placement: .navigationBarDrawer(displayMode: .always), prompt: "Search for a book")
+        SearchBody()
+            .navigationTitle(NSLocalizedString("ui.search.title", value: "Search", comment: "title for the Search screen"))
+            .searchable(text: $searchText, placement: .navigationBarDrawer(displayMode: .always), prompt: "Search for a book")
     }
 }
 
