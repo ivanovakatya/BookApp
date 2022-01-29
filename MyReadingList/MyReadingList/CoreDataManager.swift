21a788a9ee286de7aeeff0d9934d799d5f2ea144
ivanovakatya <katyaivanova0799@gmail.com>
2022-01-29 09:16:26 +0000

    Add CoreDataManager class

---
+++ MyReadingList/MyReadingList/CoreDataManager.swift
@@ -0,0 +1,13 @@
+//
+//  CoreDataManager.swift
+//  MyReadingList
+//
+//  Created by MacBook on 29/01/2022.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataManager {
+    let persistentContainer: NSPersistentContainer
+}
