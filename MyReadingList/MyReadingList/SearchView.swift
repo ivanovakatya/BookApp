56f0743da931d8a5bc329abe7b713b8722c694c5
ivanovakatya <katyaivanova0799@gmail.com>
2022-01-30 09:01:42 +0000

    Add read colour to the temoporary button

--- MyReadingList/MyReadingList/SearchView.swift
+++ MyReadingList/MyReadingList/SearchView.swift
@@ -18,10 +18,11 @@ struct SearchView: View {
         SearchBody()
             .navigationTitle(NSLocalizedString("ui.search.title", value: "Search", comment: "title for the Search screen"))
             .searchable(text: $searchText, placement: .navigationBarDrawer(displayMode: .always), prompt: "Search for a book")
+        // Add teporary button to be able to save and print the searched title
         Button("Enter") {
-            coreDataManager.saveBook(title: title)
-            print("Searched book is: \(title)")
-        }
+            coreDataManager.saveBook(title: searchText)
+            print("Searched book is: \(searchText)")
+        }.foregroundColor(Color.red)
         Spacer()
     }
 }
