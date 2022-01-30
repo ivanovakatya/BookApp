3f6056ffed90408499ebbccd7e10731894492e99
ivanovakatya <katyaivanova0799@gmail.com>
2022-01-30 08:53:50 +0000

    Revert the change of the variable name 'searchText'

--- MyReadingList/MyReadingList/SearchView.swift
+++ MyReadingList/MyReadingList/SearchView.swift
@@ -11,12 +11,13 @@ import SwiftUI
 struct SearchView: View {
     @Environment(\.isSearching) var isSearching
     let coreDataManager: CoreDataManager
-    @State private var title = ""
+    @State private var searchText = ""
+
     
     var body: some View {
         SearchBody()
             .navigationTitle(NSLocalizedString("ui.search.title", value: "Search", comment: "title for the Search screen"))
-            .searchable(text: $title, placement: .navigationBarDrawer(displayMode: .always), prompt: "Search for a book")
+            .searchable(text: $searchText, placement: .navigationBarDrawer(displayMode: .always), prompt: "Search for a book")
         Button("Enter") {
             coreDataManager.saveBook(title: title)
             print("Searched book is: \(title)")
